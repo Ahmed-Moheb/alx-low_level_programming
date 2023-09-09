cfd54556bdcf78f020d288c4a47dd697a924e44a
Ahmed-Moheb <ahmedd.mohebb2004@gmail.com>
2023-09-09 03:13:23 +0300

    h

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	for (int num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+
+	for (int hi = 97; hi <= 102; hi++)
+	{
+		putchar(hi);
+	}
+	putchar('\n');
+	return (0);
+}
